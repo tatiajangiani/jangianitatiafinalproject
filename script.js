42bec3c502af2a77eda22400f37c46fd34546fdd
tatiajangiani <153165677+tatiajangiani@users.noreply.github.com>
2024-01-19 18:15:48 +0400

    add burger menu

---
+++ script.js
@@ -0,0 +1,15 @@
+let burger = document.querySelector(".burger");
+let xmark = document.querySelector(".xmark");
+let mobileMenu = document.querySelector(".header_nav");
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.add("show");
+  xmark.style.display = "block";
+  burger.style.display = "none";
+});
+
+xmark.addEventListener("click", () => {
+  mobileMenu.classList.remove("show");
+  xmark.style.display = "none";
+  burger.style.display = "block";
+});
